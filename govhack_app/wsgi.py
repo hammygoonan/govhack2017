47790af60703000993654549093b9e24d83d9012
Hammy Goonan <hammy@falconry.io>
2017-07-30 09:59:12 +1000

    fixing wsgi script

--- govhack_app/wsgi.py
+++ govhack_app/wsgi.py
@@ -1,5 +1,6 @@
 from govhack_app import create_app
 
+application = create_app('config.production')
+
 if __name__ == "__main__":
-    application = create_app('config.production')
     application.run()
